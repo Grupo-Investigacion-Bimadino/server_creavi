b33171f40c092ec5e7fdcb0f89df3e06eef36f63
Alexander Toscano Ricardo <atoscano@correo.unicordoba.edu.co>
2024-07-25 18:44:18 -0500

    Fix login on server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { RedisIoAdapter } from './redis-io/redis-io.adapter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Redis Websockets
+  // const redisIoAdapter = new RedisIoAdapter(app);
+  // await redisIoAdapter.connectToRedis();
+
+  // app.useWebSocketAdapter(redisIoAdapter);
+  await app.listen(4000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
